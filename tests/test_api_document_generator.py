0099f7ee3123e3be56e5a257bb6e869491c1e41f
Gzp_ <741424975@qq.com>
2019-05-29 23:12:48 +0800

    [MOD] Update module construction

--- tests/test_api_document_generator.py
+++ tests/test_api_document_generator.py
@@ -4,13 +4,13 @@ __author__ = 'gzp'
 import unittest
 import json
 
-from rimuru.core import APIDocumentGenerator
+from rimuru import APIDocGenerator
 
 
-class APIDocumentGeneratorTestCase(unittest.TestCase):
+class APIDocGeneratorTestCase(unittest.TestCase):
     def setUp(self):
         self.api_name = 'Test API'
-        self.api_doc_gen = APIDocumentGenerator(method='get', url='/api/test', name=self.api_name)
+        self.api_doc_gen = APIDocGenerator(method='get', url='/api/test', name=self.api_name)
         self.api_doc_gen.add_headers('Authorization', 'JWT eyJ0eXAiOiJKV...63XjrurF1bDlv478')
         self.api_doc_gen.add_params('test', 'test')
         self.api_doc_gen.add_response(200, json.dumps({'msg': 'success'}), body_type='json')
